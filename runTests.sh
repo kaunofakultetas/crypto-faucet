5ffb6c7299993d2f92b5f25813ec092fd171c2c3
Tomas Vanagas <tomas.vanagas@knf.vu.lt>
2026-08-12 12:13:30 +0300

    Run tests on prod

--- runTests.sh
+++ runTests.sh
@@ -2,9 +2,4 @@
 set -e
 
 # Run the tests
-sudo docker build -t faucet-backend ./backend
-sudo -n docker run --rm \
-    -v $PWD/backend:/app \
-    -v $PWD/_CONFIG:/config:ro \
-    -w /app faucet-backend \
-    python -m unittest discover -s tests
+sudo docker exec -w /app faucet-backend python -m unittest discover -s tests
